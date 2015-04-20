315a680142bc83bfaff1e96cf06b15e0672cc797
ak3ake <P.Tungjitsirisun@gmail.com>
2015-04-20 15:08:22 +0700

    program1 latest modified by USER_A

--- program1.c
+++ program1.c
@@ -10,9 +10,13 @@ int main()
 	printf("Hello World!! From USER A: ak3ake\n");
 	printf("My ID: 55070501024\n");	
 
+	printf("First edit by USER_A\n");		//First edit USER_A
+
 	printf("What is your name?\n");
 	for(i=0;i<10;i++)	
 		scanf("%c",array[i]);
+
+	printf("Second edit by USER_A\n");		//Second edit USER_A
 
 	return 0;
 }
